4a1d1a88b2fe40ce6e21b43b04a1310aa4193ba2
Akshay Patel <akshayblevel@gmail.com>
2024-03-25 15:20:24 +0530

    Initial Commit

---
+++ Dotnet8-DockerProfile/VehicleDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Dotnet8_DockerProfile
+{
+    public class VehicleDb : DbContext
+    {
+        public VehicleDb(DbContextOptions<VehicleDb> options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
